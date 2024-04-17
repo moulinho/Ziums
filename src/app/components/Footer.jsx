0c73f47dcd7ef2415f1ff4fbcdf4af1327b6c14c
Abdoulaye SEREME <abdoulayesereme@192.168.1.4>
2024-04-17 22:22:41 +0000

    retouch footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,17 +4,17 @@ import { FaFacebook, FaTiktok, FaInstagram } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer>
-      <Link className="footer-link" href="/contact">
+    <footer className="row mx-3"> 
+      <Link className="footer-link col-3" href="/contact">
         CONTACT
       </Link>
-      <Link className="footer-link" href="/terms">
+      <Link className="footer-link col-3" href="/terms">
         TERMS
       </Link>
-      <div className="footer-link row" href="#">
-        <FaFacebook className="col-2" size="1.8rem" role="button" />
-        <FaTiktok className="col-2" size="1.8rem" role="button"/>
-        <FaInstagram className="col-2" size="1.8rem" role="button"/>
+      <div className="footer-link col-6  row" href="#">
+        <FaFacebook className="col-3" size="1.8rem" role="button" />
+        <FaTiktok className="col-3" size="1.8rem" role="button"/>
+        <FaInstagram className="col-3" size="1.8rem" role="button"/>
       </div>
     </footer>
   );
